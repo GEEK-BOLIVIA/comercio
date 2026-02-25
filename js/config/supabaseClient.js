6f04670cef5b8d4c360a2e47906d3e181d875acd
isaiasreciniflores-code <isaiasreciniflores@gmail.com>
2026-02-25 18:04:01 -0400

    Login

--- js/config/supabaseClient.js
+++ js/config/supabaseClient.js
@@ -1,4 +1,12 @@
 const supabaseUrl = 'https://bhbbjvcoujxzqaqjqnti.supabase.co'; 
 const supabaseAnonKey = 'sb_publishable_C_OFBl6KdorQVKjnHFBUyg_DamW_LmK'; 
-const clientInstance = supabase.createClient(supabaseUrl, supabaseAnonKey);
+
+const clientInstance = supabase.createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        persistSession: true,      // Guarda la sesión en localStorage
+        detectSessionInUrl: true,  // ¡VITAL! Lee el token de la URL de GitHub Pages
+        autoRefreshToken: true     // Mantiene la sesión activa
+    }
+});
+
 export { clientInstance as supabase };
